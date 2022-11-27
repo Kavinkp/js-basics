68c325c0fe36b774ac0eb17cc4c3450d3fe4714d
Kavin <kavinkp022@gmail.com>
2022-11-27 08:47:50 +0000

    Create 4 files

---
+++ hello.js
@@ -0,0 +1,21 @@
+function square(x) {
+    // local variable x
+    return x * x;
+}
+
+const a = 10;
+const b = 12;
+
+if(a>b){
+    let c = true;
+    console.log("a is greater", c);
+    
+} else {
+    console.log("b is greater");
+    
+}
+console.log(c);
+
+
+
+
